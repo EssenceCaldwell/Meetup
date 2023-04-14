3278dc3b75d09de36fadb42e322e9477e17936cc
EssenceCaldewll <essentialsarang@icloud.com>
2023-04-13 22:40:26 -0400

    Add User authentication Express middleware

--- backend/db/seeders/20230413032346-demo-user.js
+++ backend/db/seeders/20230413032346-demo-user.js
@@ -1,50 +1,34 @@
 'use strict';
-
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 let options = {};
-if(process.env.NODE_ENV === 'production'){
-  options.schema = process.env.SCHEMA;
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-   await queryInterface.bulkInsert('Users', [
-    {
-    email: 'demo1@demo.com',
-    username: 'demoUser1',
-    hashedPassword: bcrypt.hashSync('demoPassword1')
-   },
-   {
-    email: 'demo2@demo.com',
-    username: 'demoUser2',
-    hashedPassword: bcrypt.hashSync('demoPassword2')
-   },
-   {
-    email: 'demo3@demo.com',
-    username: 'demoUser3',
-    hashedPassword: bcrypt.hashSync('demoPassword3')
-   },
-  ])
+  up: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users';
+    return queryInterface.bulkInsert(options, [
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user1@user.io',
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'user2@user.io',
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password3')
+      }
+    ], {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => {
     options.tableName = 'Users';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
