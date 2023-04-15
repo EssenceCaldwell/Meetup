b5c19abe19ce752eca545ecb320906da13f501c0
EssenceCaldewll <essentialsarang@icloud.com>
2023-04-15 13:25:34 -0400

    Add a backend endpoint to get the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -41,7 +41,7 @@ router.post(
       });
     }
   );
-  
+
   router.delete(
     '/',
     (_req, res) => {
@@ -50,6 +50,22 @@ router.post(
     }
   );
 
+  router.get(
+    '/',
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        const safeUser = {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        };
+        return res.json({
+          user: safeUser
+        });
+      } else return res.json({ user: null });
+    }
+  );
 
 
 
