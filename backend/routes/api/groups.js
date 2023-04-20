7bbb121db09c825c5e0663ece8a1ccaa27c49c41
EssenceCaldewll <essentialsarang@icloud.com>
2023-04-19 22:29:55 -0400

    get all groups route

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,3 +1,16 @@
 const express = require('express')
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User, Group } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+router.get('', async (req, res)=>{
+const allGroups = await Group.findAll();
+res.json(allGroups)
+})
+
+
+module.exports = router
