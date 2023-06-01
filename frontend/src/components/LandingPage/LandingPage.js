292f7d4ef11ce57c136f48758eeeccab6bd76da1
EssenceCaldewll <essentialsarang@icloud.com>
2023-06-01 00:49:25 -0400

    made small changes to landing page

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -2,16 +2,52 @@ import { Link } from 'react-router-dom';
 import { allGroups } from "../../store/groups";
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route, useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import Groups from '../Groups/Groups';
 import './LandingPage.css'
-import frendlyImage from '../../Images/computer.png'
-import groupPic from '../../Images/group-of-students.png'
-import partyPic from '../../Images/party.png'
-import invitePic from '../../Images/invite.png'
+import frendlyImage from '../../Images/mainImg.png'
+import groupPic from '../../Images/joinGroup.png'
+import partyPic from '../../Images/findEvent.png'
+import invitePic from '../../Images/makeGroup.png'
 
 
-const LandingPage = () => {
+const LandingPage = ({isLoaded}) => {
+const sessionUser = useSelector((state) => state.session.user);
+const history = useHistory();
+
+const handleClickSignup = () =>{
+history.push('/signup')
+}
+
+const handleClickCreateGroup = (e) => {
+  e.preventDefault()
+
+  if(sessionUser){
+    history.push('/groups')
+  }
+}
+let joinButton;
+let createGroupsLink
+
+  if (!sessionUser) {
+    joinButton = (
+      <button className="buttons" onClick={handleClickSignup}>
+        Join What's Up
+      </button>
+    );
+    createGroupsLink =( <span className='notLink'>Start a group</span>);
+  }else createGroupsLink = (
+    <Link
+      to="/groups"
+      style={{
+        textDecoration: "none",
+        color: "teal",
+        fontFamily: "Arial",
+      }}
+    >
+      Start a group
+    </Link>
+  );
 
     return (
       <>
@@ -38,49 +74,85 @@ const LandingPage = () => {
           <div className="group-of-three">
             <div>
               <div className="innerBox">
-                <div className='innerBox'>
-                  <img src={groupPic} alt="Group of Friends" className='group-of-three-container' style={{height:200}}/>
-                  <span className='link'>
-                  <Link to="/groups" style={{textDecoration: 'none', color: 'teal', fontFamily: 'Arial'}}>Join a group</Link>
+                <div className="innerBox">
+                  <img
+                    src={groupPic}
+                    alt="Group of Friends"
+                    className="group-of-three-container"
+                    style={{ height: 150 }}
+                  />
+                  <span className="link">
+                    <Link
+                      to="/groups"
+                      style={{
+                        textDecoration: "none",
+                        color: "teal",
+                        fontFamily: "Arial",
+                      }}
+                      onClick={handleClickCreateGroup}
+                    >
+                      Join a group
+                    </Link>
                   </span>
                 </div>
 
-                <div>
-                Do what you love, meet others who love it, find your community.
-                The rest is history!
+                <div className="description">
+                  Do what you love, meet others who love it, find your
+                  community. The rest is history!
                 </div>
-
               </div>
             </div>
 
             <div className="innerBox">
               <div className="innerBox">
-              <img src={partyPic} alt="Group of Friends Partying" className='group-of-three-container' style={{height:200}}/>
-              <span className='link'>
-                <Link to="/events"style={{textDecoration: 'none', color: 'teal', fontFamily: 'Arial'}} >Find an event</Link>
-              </span>
+                <img
+                  src={partyPic}
+                  alt="Group of Friends Partying"
+                  className="group-of-three-container"
+                  style={{ height: 150 }}
+                />
+                <span className="link">
+                  <Link
+                    to="/events"
+                    style={{
+                      textDecoration: "none",
+                      color: "teal",
+                      fontFamily: "Arial",
+                    }}
+                  >
+                    Find an event
+                  </Link>
+                </span>
               </div>
 
-              <div>
-              Events are happening on just about any topic you can think of,
-              from online gaming and photography to yoga and hiking.
+              <div className="description">
+                Events are happening on just about any topic you can think of,
+                from online gaming and photography to yoga and hiking.
               </div>
             </div>
 
             <div className="innerBox">
-               <div className="innerBox">
-                <img src={invitePic} alt="Two People Saying Join Us" className='group-of-three-container' style={{height:200}}/>
-                <span className='link'>
-               <Link to="/groups" style={{textDecoration: 'none', color: 'teal', fontFamily: 'Arial'}}>Start a group</Link>
+              <div className="innerBox">
+                <img
+                  src={invitePic}
+                  alt="Two People Saying Join Us"
+                  className="group-of-three-container"
+                  style={{ height: 160 }}
+                />
+                <span className="link">
+                  {createGroupsLink}
                 </span>
-                </div>
+              </div>
 
-                <div className='description'>
+              <div className="description">
                 You don’t have to be an expert to gather people together and
                 explore shared interests.
-               </div>
+              </div>
             </div>
           </div>
+        </div>
+        <div className="joinButton">
+          {joinButton}
         </div>
       </>
     );
