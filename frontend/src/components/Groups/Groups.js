76b2f0409b076bd898780e67347c033334b70966
EssenceCaldewll <essentialsarang@icloud.com>
2023-06-02 13:34:27 -0400

    added counter for events per group

--- frontend/src/components/Groups/Groups.js
+++ frontend/src/components/Groups/Groups.js
@@ -1,20 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { allGroups } from "../../store/groups";
 import { Link } from 'react-router-dom';
 import './Groups.css';
+import { allEvents, eventsByGroup } from "../../store/events";
 
 const Groups = () => {
   const dispatch = useDispatch();
- const groupData = Object.values(useSelector((state) => state.groupState));
-    //const groups = Object.values(groupData.Groups)
+  const groupData = Object.values(useSelector((state) => state.groupState));
+  const events = Object.values(useSelector((state) => state.eventState));
+
 
   useEffect(() => {
     dispatch(allGroups());
   }, [dispatch]);
 
+  useEffect(() => {
+      dispatch(allEvents());
 
-//console.log(groupData[0])
+  }, [ dispatch]);
+
+
   return (
     <>
       <div className="container">
@@ -50,7 +56,9 @@ const Groups = () => {
           </div>
 
           <ul>
-            {groupData.map((group) => (
+            {groupData.map((group) => {
+              const groupEvents = events.filter((event) => event.groupId === group.id);
+            return (
               <li
                 className="borders"
                 onClick={() =>
@@ -70,12 +78,14 @@ const Groups = () => {
                   <h6 className="location">{`${group.city}, ${group.state}`}</h6>
                   <div className="text-width">{`${group.about}`}</div>
                   <div className="header">
-                    <h6 className="location">events</h6>
+                    {<h6 className="location">
+                      {groupEvents.length} Event(s)
+                    </h6>}
                     <h6 className="location">{`${group.type}`}</h6>
                   </div>
                 </div>
               </li>
-            ))}
+            )})}
           </ul>
         </div>
         <div></div>
--- frontend/src/store/events.js
+++ frontend/src/store/events.js
@@ -1,9 +1,21 @@
-const GET_EVENTS = '/events'
+const GET_EVENTS = '/events';
+const GET_EVENTS_FOR_GROUP = "/events/by-groupid";
 
 const getEvents = (events) => {
     return{
         type: GET_EVENTS,
         events
+    }
+}
+
+
+export const eventsByGroup = (groupId) => async (dispatch) => {
+    const response = await fetch(`/groups/${groupId}/events`);
+
+    if(response.ok){
+        const events = await response.json();
+        dispatch(getEvents(events))
+        return events
     }
 }
 
@@ -20,20 +32,20 @@ export const allEvents = () => async (dispatch) => {
 const initialState = {}
 
 const eventsReducer = (state = initialState, action) => {
-    switch(action.type){
-        case GET_EVENTS: {
-            let newState = {}
-            const events = Object.values(action.events)
-            events.forEach(event => {
-                const newEvent = Object.values(event)
-                newEvent.forEach(ele => {
-                    newState[ele.id] = ele
-                })
-            });
-            return newState
-        }
-        default:
-            return state
+    switch (action.type) {
+      case GET_EVENTS: {
+        let newState = {};
+        const events = Object.values(action.events);
+        events.forEach((event) => {
+          const newEvent = Object.values(event);
+          newEvent.forEach((ele) => {
+            newState[ele.id] = ele;
+          });
+        });
+        return newState;
+      }
+      default:
+        return state;
     }
 }
 
