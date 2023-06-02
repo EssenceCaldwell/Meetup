3bdb21e1842e98d0d2521e76735a55b7316ee9ef
EssenceCaldewll <essentialsarang@icloud.com>
2023-06-02 01:01:27 -0400

    made changes to seeder as well as set up basic format for groups list

--- frontend/src/components/Groups/Groups.js
+++ frontend/src/components/Groups/Groups.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { allGroups } from "../../store/groups";
+import { Link } from 'react-router-dom';
+import './Groups.css'
 
 const Groups = () => {
   const dispatch = useDispatch();
@@ -15,9 +17,42 @@ const Groups = () => {
 //console.log(groupData[0])
   return (
     <>
-    <ul>
-        {groupData.map(group => <li>{`${group.name}`}</li>)}
-    </ul>
+      <div className="container">
+        <div></div>
+        <div>
+          <div className=" borders">
+            <div className="header top-spacing">
+              <span>
+                <Link
+                  to="/events"
+                  style={{ textDecoration: "none", color: "black"}}>
+                  Events
+                </Link>
+              </span>
+              <span
+                className="left-spacing"
+                style={{ textDecoration: "underline", color: "teal" }}
+              >
+                Groups
+              </span>
+            </div>
+
+            <h6 className="header" style={{ color: "gray" }}>
+              Groups in What's Up
+            </h6>
+          </div>
+
+          <ul>
+            {groupData.map((group) => (
+              <li className="borders">
+                <div>{`${group.previewImage}`}</div>
+                <div>{`${group.name}`}</div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div></div>
+      </div>
     </>
   );
 };
