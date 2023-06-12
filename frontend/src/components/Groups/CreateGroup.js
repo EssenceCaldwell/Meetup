4757dfcc42c1eb482d765b8b6cdade8b215f3554
EssenceCaldewll <essentialsarang@icloud.com>
2023-06-11 21:55:36 -0400

    made a couple of changes to create group css

--- frontend/src/components/Groups/CreateGroup.js
+++ frontend/src/components/Groups/CreateGroup.js
@@ -71,22 +71,29 @@ const onSubmit = (e) => {
 
 return (
   <>
-    <div className="form-container">
-      <div></div>
-      <div className="form-left-padding">
+    <div className="create-group-container">
+      <div>
         <form onSubmit={onSubmit}>
-          <div>
-            <div>BECOME AN ORGANIZER</div>
-            <div>
+          <div style={{ borderBottom: "2px solid black" }}>
+            <div style={{ color: "teal" }}>BECOME AN ORGANIZER</div>
+            <div className="sub-header">
               We'll walk you through a few steps to build your local community
             </div>
           </div>
-          <div>
-            <div>First, set your group's location</div>
+
+          <div style={{ borderBottom: "2px solid black" }}>
+            <div className="sub-header">First, set your group's location</div>
             <label htmlFor="locationInput">
-              <div>
-                Meetup groups meet locally, in person and online. We'll connect
-                you with people in your area, and more can join you online.
+              <div
+                style={{
+                  width: "580px",
+                  paddingBottom: "20px",
+                  height: "25px",
+                }}
+              >
+                What's up groups meet locally, in person and online. We'll
+                connect you with people in your area, and more can join you
+                online.
               </div>
 
               <input
@@ -94,17 +101,19 @@ return (
                 type="text"
                 onChange={handleLocation}
                 placeholder={`City, STATE`}
-                className="invisibleInk"
+                className="text-box-background"
+                style={{ width: "325px", color: "black", height: "25px" }}
               ></input>
               <p className="errors">
                 {afterSubmit && validationErrors.location}
               </p>
             </label>
           </div>
-          <div>
-            <div>What will your group's name be?</div>
+
+          <div style={{ borderBottom: "2px solid black" }}>
+            <div className="sub-header">What will your group's name be?</div>
             <label>
-              <div>
+              <div style={{ width: "570px", paddingBottom: "20px" }}>
                 Choose a name that will give people a clear idea of what the
                 group is about. Feel free to get creative! You can edit this
                 later if you change your mind.
@@ -114,77 +123,107 @@ return (
                 type="text"
                 onChange={(e) => setName(e.target.value)}
                 placeholder={"What is your group name?"}
-                className="invisibleInk"
+                className="text-box-background"
+                style={{ width: "325px", color: "black", height: "25px" }}
               ></input>
               <p className="errors">{afterSubmit && validationErrors.name}</p>
             </label>
           </div>
-          <div>
-            <div>Now describe what your group will be about</div>
+
+          <div style={{ borderBottom: "2px solid black" }}>
+            <div className="sub-header">
+              Now describe what your group will be about
+            </div>
             <label htmlFor="about">
-              <div>
+              <div style={{ paddingBottom: "20px" }}>
                 People will see this when we promote your group, but you'll be
                 able to add to it later, too.
+              </div>
+              <div style={{ paddingBottom: "20px" }}>
+                <div>1. What's the purpose of the group?</div>
+
+                <div>2. Who should join?</div>
+
+                <div>3. What will you do at your events?</div>
               </div>
               <input
                 id="about"
                 type="text"
                 onChange={(e) => setAbout(e.target.value)}
                 placeholder={"Please write at least 30 charaters"}
-                className="invisibleInk"
+                className="text-box-background"
+                style={{
+                  width: "325px",
+                  paddingBottom: "200px",
+                  color: "black",
+                }}
               ></input>
               <p className="errors">{afterSubmit && validationErrors.about}</p>
             </label>
           </div>
-          <div>
-            <div>Final steps...</div>
+
+          <div style={{ borderBottom: "2px solid black" }}>
+            <div className="sub-header">Final steps...</div>
             <label htmlFor="type">
               Is this an in person or online group?
-              <select
-                name="type"
-                onChange={(e) => setType(e.target.value)}
-                value={type}
-                className="invisibleInk"
-              >
-                <option value="" disabled>
-                  (select one)
-                </option>
-                <option>In person</option>
-                <option>Online</option>
-              </select>
+              <div style={{ paddingTop: "10px" }}>
+                <select
+                  name="type"
+                  onChange={(e) => setType(e.target.value)}
+                  value={type}
+                  className="text-box-background"
+                  style={{ height: "25px", color: "gray" }}
+                >
+                  <option value="" disabled>
+                    (select one)
+                  </option>
+                  <option>In person</option>
+                  <option>Online</option>
+                </select>
+              </div>
               <p className="errors">{afterSubmit && validationErrors.type}</p>
             </label>
             <label htmlFor="isPrivate">
               Is this group private or public?
-              <select
-                name="isPrivate"
-                onChange={(e) => setIsPrivate(e.target.value)}
-                value={isPrivate}
-                className="invisibleInk"
-              >
-                <option value="" disabled>
-                  (select one)
-                </option>
-                <option value="true">Private</option>
-                <option value="false"> Public</option>
-              </select>
+              <div style={{ paddingTop: "10px" }}>
+                <select
+                  name="isPrivate"
+                  onChange={(e) => setIsPrivate(e.target.value)}
+                  value={isPrivate}
+                  className="text-box-background"
+                  style={{ height: "25px", color: "gray" }}
+                >
+                  <option value="" disabled>
+                    (select one)
+                  </option>
+                  <option value="true">Private</option>
+                  <option value="false"> Public</option>
+                </select>
+              </div>
               <p className="errors">
                 {afterSubmit && validationErrors.private}
               </p>
             </label>
             <label htmlFor="url">
               Please add an image url for your group below:
-              <input
-                id="url"
-                type="text"
-                onChange={(e) => setUrl(e.target.value)}
-                placeholder={"Image Url"}
-                className="invisibleInk"
-              ></input>
+              <div style={{ paddingTop: "10px" }}>
+                <input
+                  id="url"
+                  type="text"
+                  onChange={(e) => setUrl(e.target.value)}
+                  placeholder={"Image Url"}
+                  className="text-box-background"
+                  style={{ width: "325px", height: "25px", color: "black" }}
+                ></input>
+              </div>
               <p className="errors">{afterSubmit && validationErrors.url}</p>
             </label>
           </div>
-          <button type="submit">Create group</button>
+          <div style={{ paddingTop: "20px", paddingBottom: "60px" }}>
+            <button className="create-button" type="submit">
+              Create group
+            </button>
+          </div>
         </form>
       </div>
       <div></div>
